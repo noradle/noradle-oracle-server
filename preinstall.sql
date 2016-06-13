381ffeaf19b11438aabf3dea6a311de0802c2d75
Li Yong <kaven276@vip.sina.com>
2016-06-14 06:45:35 +0800

    standardize to grant_user.sql over grant2psp

--- preinstall.sql
+++ preinstall.sql
@@ -1,5 +1,5 @@
 prompt prepare Installing Noradle(psp.web) engine software to schema "&pspdbu",
 pause press enter to continue ...
 @@create_user.sql
-@@grant2psp.sql
+@@grant_user.sql
 alter session set current_schema = &pspdbu;
